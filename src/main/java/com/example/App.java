6eab54fbdf11b650d6eee6e223c4ed957ad725db
victorio99891 <Warszawa12>
2020-01-31 14:21:51 +0100

    Add stop watch to measure execution time.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,12 +3,19 @@ package com.example;
 
 import com.example.cli.RunnerCLI;
 import com.example.cli.flow.SystemExiter;
+import com.google.common.base.Stopwatch;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
         try {
             RunnerCLI.getInstance().run(args);
         } catch (SystemExiter.UnexpectedExitException e) {
@@ -19,5 +26,13 @@ public class App {
             e.printStackTrace();
 //            log.error("\n{}", ExceptionUtils.getStackTrace(e.getCause()));
         }
+
+        stopwatch.stop();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        log.info("Execution time: " + formatter.format(stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+
     }
 }
